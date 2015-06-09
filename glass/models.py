beb015b48af92909c6d303a649b936406e732f55
David Cortez <david97ly@hotmail.com>
2015-06-08 22:28:48 -0600

    todo a la normalidad

--- glass/models.py
+++ glass/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
+class Fotos(models.Model):
+	nombre = models.CharField(max_length=500)
+	ruta = models.ImageField(upload_to='photos')
 
 class Slide(models.Model):
+	foto = models.ForeignKey(Fotos)
 	mensaje = models.CharField(max_length=500)
 	submensaje = models.CharField(max_length=500)
 	valida = models.BooleanField(default=False)
@@ -18,6 +21,7 @@ class Info(models.Model):
 	titulo = models.CharField(max_length=500)
 	subtitulo = models.CharField(max_length=500)
 	informacion = models.CharField(max_length=100)
+	foto = models.ForeignKey(Fotos)
 	valida = models.BooleanField(default=False)
 	
 class Contactos(models.Model):
@@ -31,6 +35,7 @@ class Contactos(models.Model):
 	fax2 = models.CharField(max_length=500)
 	
 class Servicios(models.Model):
+	foto = models.ForeignKey(Fotos)
 	titulo = models.CharField(max_length=500)
 	informacion = models.CharField(max_length=100)
 	valida = models.BooleanField(default=False)
